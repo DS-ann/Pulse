b66e111daef1dc50aa25e784f6aae9fc8336ed89
Ashutosh Pathak <ashutoshpathakab@gmail.com>
2026-08-01 17:29:43 +0530

    fix: completely remove listenOptions to prevent aggressive speech cutoff bug

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -280,7 +280,6 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
 
     setState(() => _isListening = true);
     _speechToText.listen(
-      listenOptions: SpeechListenOptions(pauseFor: const Duration(seconds: 1)),
       onResult: (result) {
         _controller.text = result.recognizedWords;
         _controller.selection = TextSelection.fromPosition(
